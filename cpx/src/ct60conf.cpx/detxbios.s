6ffdce5e41a9e92aa0af7ec440adb9739d0fc40b
Didier Méquignon <aniplay@wanadoo.fr>
2003-06-27 01:14:22 +1000

    2003-06-27 0.98b ---------------- - Add test inside Pexec for start programs packed by Ice without crashes (file gemdos2.S).

--- cpx/src/ct60conf.cpx/detxbios.s
+++ cpx/src/ct60conf.cpx/detxbios.s
@@ -27,42 +27,91 @@ ct60_flush_cache_bis equ $c6d
 det_xbios:
 
 	move.l USP,A0
-	btst #5,(SP)				;call in supervisor state
+	btst #5,(SP)				; call in supervisor state
 	beq.s .2
 	lea.l 6(SP),A0
 	tst.w proc_type
 	beq.s .2
-	tst.w (A0)+					;if > 68000
-.2:	move.w (A0),D0				;function
-	cmp.w #$27,D0				;Puntaes
+	tst.w (A0)+					; if > 68000
+.2:	move.w (A0),D0				; function
+	cmp.w #$27,D0				; Puntaes
 	bne.s .8
-	cmp.l #'AnKr',2(A0)			;MagiC
+	cmp.l #'AnKr',2(A0)			; MagiC
 	bne .1
-	cmp.w #-1,6(A0)				;added for the CT60
+	cmp.w #-1,6(A0)				; added for the CT60
 	bne .1
 	jsr ct60_stop
 	rte
-.8:	cmp.w #$40,D0				;Blitmode
-	bne.s .6
+.8:	cmp.w #$40,D0			 	; Blitmode
+	bne.s .17
 	moveq #0,D0
 	rte
+.17:
+	cmp.w #160,D0               ; CacheCtrl MilanTOS
+	bne.s .6
+	move.w 2(A0),D0             ; OpCode
+	bne.s .18
+	moveq #0,D0                 ; function is implemented
+	rte
+.18:
+	cmp.w #1,D0                 ; flush data cache
+	bne.s .19
+	cpusha DC
+	moveq #0,D0
+	rte	
+.19:
+	cmp.w #2,D0                 ; flush instruction cache
+	bne.s .20
+	cpusha IC
+	moveq #0,D0
+	rte
+.20:
+	cmp.w #3,D0                 ; flush data and instruction caches
+	beq .16
+	cmp.w #4,D0                 ; inquire data cache mode
+	bne.s .21
+	movec.l CACR,D0
+	btst #31,D0
+	bra.s .22
+.21:
+	cmp.w #6,D0                 ; inquire instruction cache mode
+	bne.s .23
+	movec.l CACR,D0
+	btst #15,D0
+.22:
+	sne.b D0
+	and.w #1,D0
+	ext.l D0
+	rte
+.23:
+	cmp.w #5,D0                 ; set data cache mode
+	beq.s .24
+	cmp.w #7,D0                 ; set instruction cache mode
+	bne.s .25
+.24:	
+	tst.w 4(A0)                 ; mode
+	beq .26                     ; disable
+	bra .11                     ; enable
+.25:
+	moveq #-5,D0                ; error
+	rts
 .6:	cmp.w #ct60_read_core_temperature,D0
 	beq.s .13
 	cmp.w #ct60_read_core_temperature_bis,D0
 	bne.s .5
 .13:
-	move.w 2(A0),-(SP)			;deg_type
+	move.w 2(A0),-(SP)			; deg_type
 	if TEST
 		move.l temp,D0
 		subq #1,count
 		bpl.s .7
 		move #14,count
-		move #17,-(SP)			;Random
+		move #17,-(SP)			; Random
 		trap #14
 		addq #2,SP
 		and.l #7,D0
 		add.l temp,d0
-		subq.l #3,D0			;-3 … 4
+		subq.l #3,D0			; -3 … 4
 		bpl.s .7
 		moveq #0,d0
 .7:		move.l d0,temp
@@ -77,7 +126,7 @@ det_xbios:
 	divu #5,D0
 	add.w #32,D0
 	bra.s .3
-.4:	moveq #CT60_READ_ERROR,D0	;error
+.4:	moveq #CT60_READ_ERROR,D0	; error
 .3:	addq #2,SP
 	rte
 .5:	cmp.w #ct60_rw_parameter,D0
@@ -85,9 +134,9 @@ det_xbios:
 	cmp.w #ct60_rw_parameter_bis,D0
 	bne.s .9
 .14:
-	move.w 2(A0),D0                                     ; mode
-	move.l 4(A0),D1                                     ; type_param
-	move.l 8(A0),D2                                     ; value
+	move.w 2(A0),D0             ; mode
+	move.l 4(A0),D1             ; type_param
+	move.l 8(A0),D2             ; value
 	bsr ct60_rw_param 
 	rte
 .9:	cmp.w #ct60_cache,D0
@@ -98,7 +147,8 @@ det_xbios:
 	move.w 2(A0),D0
 	bmi.s .10
 	bne.s .11
-	move.w SR,-(SP)                                     ; caches off
+.26:
+	move.w SR,-(SP)             ; caches off
 	or.w #$700,SR
 	cpusha DC
 	moveq #0,D0
@@ -107,7 +157,7 @@ det_xbios:
 	move.w (SP)+,SR
 	rte	
 .11:
-	move.w SR,-(SP)                                     ; caches on
+	move.w SR,-(SP)             ; caches on
 	or.w #$700,SR
 	cpusha BC
 	move.l #$A0808000,D0
@@ -124,6 +174,7 @@ det_xbios:
 	bne.s .1
 .16:
 	cpusha BC	
+	moveq #0,D0
 	rte
 .1:	move.l det_xbios-4,-(SP)
 	rts	
@@ -135,22 +186,22 @@ det_xbios:
 det_xbios_030:
 
 	move.l USP,A0
-	btst #5,(SP)				;call in supervisor state
+	btst #5,(SP)				; call in supervisor state
 	beq.s .2
 	lea.l 6(SP),A0
 	tst.w proc_type
 	beq.s .2
-	tst.w (A0)+					;if > 68000
-.2:	move.w (A0),D0				;function
+	tst.w (A0)+					; if > 68000
+.2:	move.w (A0),D0				; function
 	cmp.w #ct60_read_core_temperature,D0
 	bne.s .5
-	moveq #CT60_READ_ERROR,D0	;error
+	moveq #CT60_READ_ERROR,D0	; error
 	rte
 .5:	cmp.w #ct60_rw_parameter,D0
 	bne.s .3
-	move.w 2(A0),D0                                     ; mode
-	move.l 4(A0),D1                                     ; type_param
-	move.l 8(A0),D2                                     ; value
+	move.w 2(A0),D0             ; mode
+	move.l 4(A0),D1             ; type_param
+	move.l 8(A0),D2             ; value
 	bsr ct60_rw_param 
 	rte
 .3:	cmp.w #ct60_cache,D0
